409860c2e8c0fee56904169b769d4ec6ff49785f
winseywu <ww2157@nyu.edu>
2025-04-09 15:59:53 -0400

    Update env.js
    more api

--- env.js
+++ env.js
@@ -1,4 +1,10 @@
 //----------API----------//
 
 let GOOGLE_API_KEY = "AIzaSyAv1J6FoMpKe3X9MzgyWLw4l7OEDnIj2eQ";
+
+
+//next one
+//let GOOGLE_API_KEY = "AIzaSyA0F6Bp_FGhnyaY1zgCchsjr3-5dKqk09k";
+
+
 //let GOOGLE_API_KEY = "AIzaSyAbOZDL2NyR6hTFzxGH2LCA7InwqEp4Psw";
