7a688b715f2dc78fa8ec579655f1f0f8ad55fecf
Winsey <ww2157@nyu.edu>
2025-04-09 11:40:39 -0400

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 //----------API----------//
 
-//let GOOGLE_API_KEY = "AIzaSyAv1J6FoMpKe3X9MzgyWLw4l7OEDnIj2eQ";
-let GOOGLE_API_KEY = "AIzaSyAbOZDL2NyR6hTFzxGH2LCA7InwqEp4Psw";
+let GOOGLE_API_KEY = "AIzaSyAv1J6FoMpKe3X9MzgyWLw4l7OEDnIj2eQ";
+//let GOOGLE_API_KEY = "AIzaSyAbOZDL2NyR6hTFzxGH2LCA7InwqEp4Psw";
