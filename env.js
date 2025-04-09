2727808fbab4164c2a8b1217c7a2d16c2655c331
Winsey <ww2157@nyu.edu>
2025-04-09 09:13:11 -0400

    Update env.js
    needed new api with diff acc

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-//----------API------------//
+//----------API----------//
 
-let GOOGLE_API_KEY = "AIzaSyAv1J6FoMpKe3X9MzgyWLw4l7OEDnIj2eQ";
-
+//let GOOGLE_API_KEY = "AIzaSyAv1J6FoMpKe3X9MzgyWLw4l7OEDnIj2eQ";
+let GOOGLE_API_KEY = "AIzaSyAbOZDL2NyR6hTFzxGH2LCA7InwqEp4Psw";
